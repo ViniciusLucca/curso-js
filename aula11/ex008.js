16ad13b7caf0222367aae2ad6811639786753729
ViniciusLucca <vinilucca7304@gmail.com>
2024-10-08 00:44:44 -0300

    create ex 008 009 010

---
+++ aula11/ex008.js
@@ -0,0 +1,7 @@
+var velocidade = 100
+console.log(`A sua velocidade é ${velocidade}km/h`)
+if (velocidade > 60)
+{
+    console.log(`Sua velocidade foi maior do que a permitida. MULTADO!`)
+}
+console.log(`Sempre diriga com segurança!`)
