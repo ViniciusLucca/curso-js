7efa585f12283674d653eac7b4f1f208de9079ef
ViniciusLucca <vinilucca7304@gmail.com>
2024-10-09 12:24:38 -0300

    update ex015

--- aula12ex/ex015/script.js
+++ aula12ex/ex015/script.js
@@ -1,15 +1,51 @@
-let idade
 let anoAtual = new Date
-let anoNasc
+let res = window.document.querySelector("p#mostrarIdade")
 anoAtual = anoAtual.getFullYear()
 function calcularIdade()
 {
-    anoNasc = window.document.querySelector("input#ianoNascimento").value
-    if (anoNasc > anoAtual || anoNasc.value.lenght == 0)
+    let anoNasc = window.document.querySelector("input#ianoNascimento").value
+    let idade = 0
+    let formsGenero = window.document.getElementsByName("genero")
+    let genero = ''
+    let imagem = window.document.getElementsByTagName("img")[0]
+    let faixaEtaria = ""
+    if (anoNasc > anoAtual || anoNasc == 0 || !(formsGenero[0].checked || formsGenero[1].checked))
     {
         alert("[ERRO] Dados inválidos.")
         return
     }
+    
     idade = 2024 - anoNasc
-    alert(idade)
+    if (formsGenero[0].checked)
+    {
+        genero = "homem"
+    }
+    if (formsGenero[1].checked)
+    {
+        genero = "mulher"
+    }
+
+    if (idade < 6) 
+    {
+        faixaEtaria = "bebe"
+    }
+    if (idade >=6 && idade < 16)
+    {
+        faixaEtaria = "crianca"
+    }
+    if (idade >=16 && idade < 36) 
+    {
+        faixaEtaria = "jovem"
+    }
+    if (idade >=36 && idade < 55)
+    {
+        faixaEtaria = "adulto"
+    }
+    if (idade >=55)
+    {
+        faixaEtaria = "idoso"
+    }
+
+    imagem.src = `imagens/${genero}-${faixaEtaria}.jpg`
+    res.innerHTML = `Detectamos ${genero} com ${idade} anos.`
 }
