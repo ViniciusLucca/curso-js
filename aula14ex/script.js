cda9cde8f920d4f6c095fcb461e463927d029fc4
ViniciusLucca <vinilucca7304@gmail.com>
2024-10-24 14:17:29 -0300

    Update script.js

--- aula14ex/script.js
+++ aula14ex/script.js
@@ -2,22 +2,33 @@ const saida = window.document.querySelector("#output")
 const btn = window.document.querySelector("#icontar")
 var inicio, fim, passo
 btn.addEventListener("click", contar)
-function contar()
-{
+function contar() {
+    if (window.document.querySelector("#iinicio").value == '' || window.document.querySelector("#ifim").value == '' || window.document.querySelector("#ipasso").value == '') {
+        window.alert("Insira valores válidos")
+        saida.innerText = "Impossivel contar"
+    }
+    else { pegarDados() }
     console.log("contar")
     pegarDados()
-    for (let i = inicio; i<=fim; i += passo)
+    if (passo <= 0)
     {
-        saida.innerHTML += `👉${i}`
+        saida.innerText = "Impossivel contar"
+        return
     }
+    if (inicio <= fim) {
+        for (let i = inicio; i <= fim; i += passo) {
+            saida.innerHTML += `👉${i}`
+        }
+
+    } else {
+        for (let i = inicio; i >= fim; i -= passo) {
+            saida.innerHTML += `👉${i}`
+        }
+    }
+
 }
-function pegarDados()
-{
-    console.log("pegar dados")
-    if (window.document.querySelector("#iinicio").value == '' || window.document.querySelector("#ifim").value == '' || window.document.querySelector("#ipasso").value == '')
-    {
-        window.alert("Insira valores válidos")
-    }
+function pegarDados() {
+
     inicio = Number(window.document.querySelector("#iinicio").value)
     fim = Number(window.document.querySelector("#ifim").value)
     passo = Number(window.document.querySelector("#ipasso").value)
